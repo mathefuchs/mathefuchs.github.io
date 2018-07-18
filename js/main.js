e69e6d3e44d46f0dad6ad9a759a3164b1d9edb7d
Tobias Fuchs <derfuchs99@outlook.de>
2018-07-18 23:17:50 +0200

    Reformat and add comments

--- js/main.js
+++ js/main.js
@@ -1,96 +1,92 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
+	// detect is mobile device
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
-	
-	var fullHeight = function() {
-
-		if ( !isMobile.any() ) {
+	// set full height
+	var fullHeight = function () {
+		if (!isMobile.any()) {
 			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
+			$(window).resize(function () {
 				$('.js-fullheight').css('height', $(window).height());
 			});
 		}
 	};
 
-	// Parallax
-	var parallax = function() {
+	// parallax
+	var parallax = function () {
 		$(window).stellar();
 	};
 
-	var contentWayPoint = function() {
+	// configure way points
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
+		$('.animate-box').waypoint(function (direction) {
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight animated-fast');
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
+						}, k * 100, 'easeInOutExpo');
 					});
-					
-				}, 50);
-				
-			}
 
-		} , { offset: '85%' } );
+				}, 50);
+			}
+		}, { offset: '85%' });
 	};
 
+	// scroll to top button action
+	var goToTop = function () {
 
+		$('.js-gotop').on('click', function (event) {
 
-	var goToTop = function() {
-
-		$('.js-gotop').on('click', function(event){
-			
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
-			
+
 			return false;
 		});
 
-		$(window).scroll(function(){
+		$(window).scroll(function () {
 
 			var $win = $(window);
 			if ($win.scrollTop() > 200) {
@@ -100,50 +96,20 @@
 			}
 
 		});
-	
-	};
-
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#00264d',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
-	var skillsWayPoint = function() {
-		if ($('#fuchs-skills').length > 0 ) {
-			$('#fuchs-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
 
 	};
-
 
 	// Loading page
-	var loaderPage = function() {
+	var loaderPage = function () {
 		$(".fuchs-loader").fadeOut("slow");
 	};
 
-	
-	$(function(){
+	$(function () {
 		contentWayPoint();
 		goToTop();
 		loaderPage();
 		fullHeight();
 		parallax();
-		// pieChart();
-		skillsWayPoint();
 	});
-
 
 }());
